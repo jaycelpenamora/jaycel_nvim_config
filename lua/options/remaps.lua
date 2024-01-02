2663db30eba62ec620357dad1eb9968f456ca3c2
jaycelpenamora <jaycelpenamora@gmail.com>
2024-01-03 04:43:01 +0800

    From laptop

--- lua/options/remaps.lua
+++ lua/options/remaps.lua
@@ -3,7 +3,7 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
+vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
