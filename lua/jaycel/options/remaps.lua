9dbcff712de5b6b0abcecd99d38c34b083c93004
jaycelpenamora <jaycelpenamora@gmail.com>
2024-01-05 21:51:16 +0800

    modified:   init.lua 	new file:   lua/jaycel/options/lspconfig.lua 	renamed:    lua/options/remaps.lua -> lua/jaycel/options/remaps.lua 	renamed:    lua/options/settings.lua -> lua/jaycel/options/settings.lua 	renamed:    lua/custom/plugins/autocompletion.lua -> lua/jaycel/plugins/autocompletion.lua 	renamed:    lua/kickstart/plugins/autoformat.lua -> lua/jaycel/plugins/autoformat.lua 	renamed:    lua/kickstart/plugins/debug.lua -> lua/jaycel/plugins/debug.lua 	renamed:    lua/custom/plugins/gitsigns.lua -> lua/jaycel/plugins/gitsigns.lua 	renamed:    lua/custom/plugins/init.lua -> lua/jaycel/plugins/init.lua 	renamed:    lua/custom/plugins/lspconfig.lua -> lua/jaycel/plugins/lsp.lua 	renamed:    lua/custom/plugins/lualine.lua -> lua/jaycel/plugins/lualine.lua 	renamed:    lua/custom/plugins/telescope.lua -> lua/jaycel/plugins/telescope.lua 	renamed:    lua/custom/plugins/themes.lua -> lua/jaycel/plugins/themes.lua 	renamed:    lua/custom/plugins/treesitter.lua -> lua/jaycel/plugins/treesitter.lua

--- lua/jaycel/options/remaps.lua
+++ lua/jaycel/options/remaps.lua
@@ -1,9 +1,9 @@
 -- [[ Basic Keymaps ]]
-
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -163,3 +163,117 @@ vim.defer_fn(function()
 		},
 	}
 end, 0)
+
+-- [[ Configure LSP ]]
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+	-- NOTE: Remember that lua is a real programming language, and as such it is possible
+	-- to define small helper and utility functions so you don't have to repeat yourself
+	-- many times.
+	--
+	-- In this case, we create a function that lets us more easily define mappings specific
+	-- for LSP related items. It sets the mode, buffer and description for us each time.
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = 'LSP: ' .. desc
+		end
+
+		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+	nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+	nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+	nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+	-- See `:help K` for why this keymap
+	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+	-- Lesser used LSP functionality
+	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+	nmap('<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, '[W]orkspace [L]ist Folders')
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+		vim.lsp.buf.format()
+	end, { desc = 'Format current buffer with LSP' })
+end
+-- document existing key chains
+require('which-key').register {
+	['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+	['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+	['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+	['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+	['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+	['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+	['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+	['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+}
+-- register which-key VISUAL mode
+-- required for visual <leader>hs (hunk stage) to work
+require('which-key').register({
+	['<leader>'] = { name = 'VISUAL <leader>' },
+	['<leader>h'] = { 'Git [H]unk' },
+}, { mode = 'v' })
+
+-- [[ Configure nvim-cmp ]]
+-- See `:help cmp`
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	mapping = cmp.mapping.preset.insert {
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete {},
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'path' },
+	},
+}
