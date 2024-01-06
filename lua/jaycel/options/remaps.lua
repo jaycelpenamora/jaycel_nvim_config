9575f1e706d1c1652367444e6939b0f6a3dbf13e
jaycelpenamora <jaycelpenamora@gmail.com>
2024-01-06 12:36:26 +0800

    modified:   init.lua 	modified:   lazy-lock.json 	modified:   lua/jaycel/options/remaps.lua 	lua/jaycel/plugins/minipairs.lua 	lua/jaycel/plugins/minisurround.lua

--- lua/jaycel/options/remaps.lua
+++ lua/jaycel/options/remaps.lua
@@ -227,17 +227,6 @@ lsp_zero.on_attach(function(client, bufnr)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	ensure_installed = { 'tsserver', 'rust_analyzer' },
-	handlers = {
-		lsp_zero.default_setup,
-		lua_ls = function()
-			local lua_opts = lsp_zero.nvim_lua_ls()
-			require('lspconfig').lua_ls.setup(lua_opts)
-		end,
-	}
-})
 -- document existing key chains
 require('which-key').register {
 	['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
@@ -256,59 +245,6 @@ require('which-key').register({
 	['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	completion = {
-		completeopt = 'menu,menuone,noinsert',
-	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		-- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete {},
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	},
-	sources = {
-		{ name = 'path' },
-		{ name = 'nvim_lsp' },
-		{ name = 'nvim_lua' },
-		{ name = 'luasnip', keyword_length = 2 },
-		{ name = 'buffer',  keyword_length = 3 },
-	},
-}
 -- [[ Harpoon Remaps ]] --
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
