a1b1c78ce52cbd095685ec0587488c2785ecb8ee
jaycelpenamora <jaycelpenamora@gmail.com>
2024-06-02 14:05:20 +0800

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -163,7 +163,6 @@ require('mason-lspconfig').setup()
 local servers = {
   clangd = {},
   gopls = {},
-  pyright = {},
   rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
