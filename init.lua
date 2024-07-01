f95108a106feb2cc208228dc84413f03629739d3
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-02 06:30:34 +0800

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -340,7 +340,11 @@ prettier.setup({
     "yaml",
   },
 })
-require("colorizer").setup()
+require("colorizer").setup({
+  '*',
+  css = { rgb_fn = true, },
+  html = { names = true, },
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
